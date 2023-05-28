d92831fef41803ed6cba9e6c9e407c9912a3f691
sid <sidrawatkot1@gmail.com>
2023-05-28 14:54:45 +0530

    high order function

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Route,Switch} from "react-router-dom"
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Register from "../auth/Register";
 import Login from "../auth/Login";
 import Alert from "../layout/Alert";
@@ -12,31 +12,31 @@ import Profiles from "../profiles/Profiles";
 import Profile from "../profile/Profile";
 import Posts from "../posts/Posts";
 import Post from "../post/Post";
-import NotFound from "../layout/NotFound"
+import NotFound from "../layout/NotFound";
 
 const Routes = () => {
-    return (
-        <section className="container">
-        <Alert />
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/profiles' component={Profiles} />
-          <Route exact path='/profile/:id' component={Profile} />
-          {/* <Route exact path='/chat' component={Chat} /> */}
-          {/* <Route exact path='/upload' component={UploadForm} /> */}
+  return (
+    <section className="container">
+      <Alert />
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/profiles" component={Profiles} />
+        <Route exact path="/profile/:id" component={Profile} />
+        {/* <Route exact path='/chat' component={Chat} /> */}
+        {/* <Route exact path='/upload' component={UploadForm} /> */}
 
-          <PrivateRoute exact path='/dashboard' component={Dashboard} />
-          <PrivateRoute exact path='/create-profile' component={ProfileForm} />
-          <PrivateRoute exact path='/edit-profile' component={ProfileForm} />
-          <PrivateRoute exact path='/add-experience' component={AddExperience} />
-          <PrivateRoute exact path='/add-education' component={AddEducation} />
-          <PrivateRoute exact path='/posts' component={Posts} />
-          <PrivateRoute exact path='/posts/:id' component={Post} />
-          <Route component={NotFound} />
-        </Switch>
-      </section>
-    )
-}
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute exact path="/create-profile" component={ProfileForm} />
+        <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
+        <PrivateRoute exact path="/add-experience" component={AddExperience} />
+        <PrivateRoute exact path="/add-education" component={AddEducation} />
+        <PrivateRoute exact path="/posts" component={Posts} />
+        <PrivateRoute exact path="/posts/:id" component={Post} />
+        <Route component={NotFound} />
+      </Switch>
+    </section>
+  ); //A higher-order component is a function that takes a component uses a condition and returns a new component
+};
 
-export default Routes
+export default Routes;
